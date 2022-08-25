fbaa545bdc873924f8a07ef23d365f23002e34f3
Robert Tian <74839761+Robertian23@users.noreply.github.com>
2022-08-25 11:43:08 -0700

    Update ahs_lab01_q4_test.py

--- ahs_lab01_tests/ahs_lab01_q4_test.py
+++ ahs_lab01_tests/ahs_lab01_q4_test.py
@@ -1,7 +1,7 @@
-from ahs_lab01 import wears_jackets_with_if
+from ahs_lab01 import wears_jacket_with_if
 
 def test():
-    assert wears_jackets_with_if(90, False) == False
-    assert wears_jackets_with_if(40, False) == True
-    assert wears_jackets_with_if(100, True) == True
+    assert wears_jacket_with_if(90, False) == False
+    assert wears_jacket_with_if(40, False) == True
+    assert wears_jacket_with_if(100, True) == True
     
