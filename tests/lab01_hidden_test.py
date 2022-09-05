829c0f6ae669d07210684a708349e0a8aeac0492
Robert Tian <74839761+Robertian23@users.noreply.github.com>
2022-09-05 16:34:22 -0700

    Add files via upload

---
+++ tests/lab01_hidden_test.py
@@ -0,0 +1,10 @@
+import labs.lab01 as lab
+import keys.lab01_key as key
+import random
+
+def hidden_test_falling():
+    for i in range(0, 20):
+        n = random.randint(0, 100)
+        k = random.randint(0, n)
+            
+
