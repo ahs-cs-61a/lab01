41b04554fe7ec8aed0c3d033e84e56f0007cdbea
sean-morris <sean.smorris@gmail.com>
2022-09-07 18:38:24 -0700

    path forward

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from labs.lab01 import *
 from labs.lab01_wwpd import * 
 
+# exec(open("main.py").read())
+# hailstone(10)
+# wwpd_control()
+
+# import labs.lab01 as ex
+# import labs.lab01_wwpd as exw
+
+# ex.hailstone(10)
+# exw.wwpd_control()
