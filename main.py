b1799a259c2b6b004fb74d4dc35315b8b6dade82
Robert Tian <roberttian0523@gmail.com>
2022-09-17 15:28:49 -0700

    minor change

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from labs.lab01_wwpd import *
 # WWPD questions:  
 # un-comment one-by-one and complete question set
 
-# wwpd_control()
 # wwpd_booleans()
+# wwpd_control()
 # wwpd_what_if()
 # wwpd_case_conundrum()
 # wwpd_square_so_slow()
